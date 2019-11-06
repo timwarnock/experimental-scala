a52a8b15b2f1a6eeeb45269702f1cf642e7766c5
Tim Warnock <timothy.warnock@capitalone.com>
2019-11-06 13:26:35 -0500

    getting started

---
+++ redbook/src/main/scala/gettingstarted/GettingStarted.scala
@@ -0,0 +1,77 @@
+package gettingstarted
+
+// A comment!
+/** A documentation comment */
+object MyModule {
+
+  def abs(n: Int): Int =
+    if (n < 0) -n
+    else n
+
+  private def formatAbs(x: Int) = {
+    val msg = "The absolute value of %d is %d"
+    msg.format(x, abs(x))
+  }
+
+  def main(args: Array[String]): Unit =
+    println(formatAbs(-42))
+
+  def factorial(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, acc: Int): Int =
+      if (n <= 0) acc
+      else go(n-1, n*acc)
+
+    go(n, 1)
+  }
+
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def loop(n: Int, prev: Int, cur: Int): Int =
+      if (n == 0) prev
+      else loop(n - 1, cur, prev + cur)
+    loop(n, 0, 1)
+  }
+  
+}
+
+object Monomorphic {
+
+  def findFirst(ss: Array[String], key: String): Int = {
+    @annotation.tailrec
+    def loop(n: Int): Int =
+      if (n >= ss.length) -1
+      else if (ss(n) == key) n
+      else loop(n + 1)
+    loop(0)
+  }
+
+}
+
+object Polymorphic {
+
+  /**  scala> Polymorphic.findFirst(Array("abc", "b", "c"), (x: String) => x == "b")
+   *   res1: Int = 1
+   */
+  def findFirst[A](as: Array[A], p: A => Boolean): Int = {
+    @annotation.tailrec
+    def loop(n: Int): Int =
+      if (n >= as.length) -1
+      else if (p(as(n))) n
+      else loop(n + 1)
+    loop(0)
+  }
+
+  /**  scala> Polymorphic.isSorted(Array(1,2,3), (x: Int, y: Int) => x > y)
+   *   res1: Boolean = true
+   */
+  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def go(n: Int): Boolean =
+      if (n >= as.length-1) true
+      else if (gt(as(n), as(n+1))) false
+      else go(n+1)
+    go(0)
+  }
+
+}
