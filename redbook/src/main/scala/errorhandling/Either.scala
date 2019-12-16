51a7ef230775a58ed6fc87c0f7cc5be174d89d29
Tim WarnockAA <tim@timwarnock.com>
2019-12-16 11:38:00 +0700

    redbook Either data type

--- redbook/src/main/scala/errorhandling/Either.scala
+++ redbook/src/main/scala/errorhandling/Either.scala
@@ -4,6 +4,35 @@ package errorhandling
 import scala.{Option => _, Either => _, _}
 
 sealed trait Either[+E,+A] {
+
+  /** 4.6
+   * scala> def circled(r: Double) = math.Pi * r * r
+   * circled: (r: Double)Double
+   *
+   * scala> Right(2.5).map(circled)
+   * res0: errorhandling.Either[Nothing,Double] = Right(19.634954084936208)
+   *
+   * scala> Right(4.2).flatMap((r: Double) => Right(math.Pi * r * r))
+   * res1: errorhandling.Either[Nothing,Double] = Right(55.41769440932395)
+   *
+   * scala> res0.getOrElse(0.0) + res1.getOrElse(0.0)
+   * res2: Double = 75.05264849426015
+   *
+   * // since we have map and flatMap we can use for-comprehension
+   * scala> for {
+   *      |  x <- Right(4.1).map(circled)
+   *      |  y <- Right(2.5).map(circled)
+   *      | } yield x + y
+   * res3: errorhandling.Either[Nothing,Double] = Right(72.44512659178062)
+   *
+   * scala> Right(4.1).map2(Right(2.5))((a,b) => circled(a) + circled(b))
+   * res4: errorhandling.Either[Nothing,Double] = Right(72.44512659178062)
+   *
+   */
+  def getOrElse[A1 >: A](or: => A1): A1 = this match {
+    case Right(a) => a
+    case _        => or
+  }
   def map[B](f: A => B): Either[E, B] =
     this match {
       case Right(a) => Right(f(a))
@@ -27,20 +56,51 @@ case class Left[+E](get: E) extends Either[E,Nothing]
 case class Right[+A](get: A) extends Either[Nothing,A]
 
 object Either {
+
+  /**
+   * scala> Either.mean(IndexedSeq(1,2,3,4))
+   * res0: errorhandling.Either[String,Double] = Right(2.5)
+   *
+   */
   def mean(xs: IndexedSeq[Double]): Either[String, Double] =
     if (xs.isEmpty)
       Left("mean of empty list!")
     else
       Right(xs.sum / xs.length)
 
+  /**
+   * scala> Either.safeDiv(42, 0)
+   * res0: errorhandling.Either[Exception,Int] = Left(java.lang.ArithmeticException: / by zero)
+   *
+   */
   def safeDiv(x: Int, y: Int): Either[Exception, Int] =
     try Right(x / y)
     catch { case e: Exception => Left(e) }
 
+  /**
+   * scala> Either.Try(42 / 0)
+   * res0: errorhandling.Either[Exception,Int] = Left(java.lang.ArithmeticException: / by zero)
+   *
+   */
   def Try[A](a: => A): Either[Exception, A] =
     try Right(a)
     catch { case e: Exception => Left(e) }
 
+  /** 4.7
+   *
+   * scala> Either.traverse(List(3,2,1))(x => Either.Try(42/x))
+   * res0: errorhandling.Either[Exception,List[Int]] = Right(List(14, 21, 42))
+   *
+   * scala> Either.traverse(List(3,2,1,0))(x => Either.Try(42/x))
+   * res1: errorhandling.Either[Exception,List[Int]] = Left(java.lang.ArithmeticException: / by zero)
+   *
+   * scala> Either.sequence(List(Right(1), Right(2), Right(3)))
+   * res2: errorhandling.Either[Nothing,List[Int]] = Right(List(1, 2, 3))
+   *
+   * scala> Either.sequence(List(Right(1), Left(2), Left(3)))
+   * res3: errorhandling.Either[Int,List[Int]] = Left(2)
+   *
+   */
   def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
     es match {
       case Nil => Right(Nil)
@@ -53,7 +113,7 @@ object Either {
   def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] =
     traverse(es)(x => x)
 
-  /*
+  /** 4.8 (from answer sheet)
   There are a number of variations on `Option` and `Either`. If we want to accumulate multiple errors, a simple
   approach is a new data type that lets us keep a list of errors in the data constructor that represents failures:
 
